f433a8197b6371bc227c609cf0ce900417f3de53
Dominik Tatrzanski <dominik.tatrzanski@capgemini.com>
2025-11-18 08:42:18 +0100

    update class for dev script

--- dev_scripts/generate_tests_for_class.py
+++ dev_scripts/generate_tests_for_class.py
@@ -10,12 +10,10 @@ def main() -> None:
     It executes the full test generation pipeline for each chunk of code.
     """
     class_path = Path(
-        r"C:\moeve_IDE\moeve-ide\workspaces\main\moeve-vvst\moeve-vvst-dlg\src"
-        r"\main\java\de\itzbund\moeve\vvst\permission"
-        r"\application\dlg\controller\OtherAdmissionsPanelControllerBean.java"
+        r"C:\Projects\moeve\R3\devon-ide\workspaces\main\moeve-enst\moeve-enst-dlg\src\main\java\de\itzbund\moeve\enst\permission\application\dlg\af100\controller\OtherAdmissionsPanelControllerBean.java"
     )
 
-    generator = TestsGenerator(extract_class_name(class_path), datetime.now().isoformat().replace(":", "-") )
+    generator = TestsGenerator(extract_class_name(class_path), datetime.now().isoformat().replace(":", "-"))
     generator.generate_tests_for_class(class_path=class_path)
 
 
